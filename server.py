4379df30b86db623a104c7a047949d306b1d493b
JakeOOS <jakecook311297@gmail.com>
2026-06-20 19:59:32 +0100

    Update server.py

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-FIRMWARE_VERSION = "12"
+FIRMWARE_VERSION = "13"
 
 OWM_API_KEY    = os.environ.get("OWM_API_KEY", "")
 RDM_API_KEY    = os.environ.get("RDM_API_KEY", "")
